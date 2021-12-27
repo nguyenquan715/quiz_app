1740d84924ebd7def2f162b84aee050f8ab5cd87
AnhNV <anhnv@relipasoft.com>
2021-12-27 17:45:23 +0700

    commit

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,47 @@
 import { useEffect, useRef, useState } from 'react';
+import '../assets/css/quiz.scss'
 
 export const Quiz = (props) => {  
-  useEffect(() => {
-
-  }, []);
-
   return (    
-    <h2>Quiz</h2>    
+    <div className="quiz">
+      <QuestionsCount index={1} total={10}/>
+      <QuestionAnswer index={1} question='Inside which HTML element do we put the JavaScript?' options={['javascript', 'scripting', 'js', 'script']}/>
+      <Pagination/>
+    </div>
+  );
+}
+
+const QuestionsCount = (props) => {
+  return (     
+    <span className="questions-count">Question {props.index} of {props.total}</span>    
+  )
+}
+
+const Pagination = (props) => {
+  return (
+    <div className="pagination">
+      <button className="btn-pagination">First</button>
+      <button className="btn-pagination">Prev</button>
+      <button className="btn-pagination btn-active">Next</button>
+      <button className="btn-pagination">Last</button>
+    </div>
+  )
+}
+
+const QuestionAnswer = (props) => {
+  return (
+    <div className='question-answer'>
+      <div className="question-content">
+        <span>{props.index}. {props.question}</span>
+      </div>
+      <div className="answers">
+        {props.options.map((option, index) => (
+          <div className="option" key={`question${props.index}-option${index}`}>
+            <input type="radio" name={`question-${props.index}`} value={index} />
+            <label>{option}</label>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,18 +1,34 @@
+import '../assets/css/result.scss';
+
 export const Result = (props) => {
   return (
-    <Question index={1} question='Inside which HTML element do we put the JavaScript?' options={['javascript', 'scripting', 'js', 'script']} isCorrect={false}/>
+    <div className="questions-board">
+      <Question index={1} question='Inside which HTML element do we put the JavaScript?' options={['javascript', 'scripting', 'js', 'script']} isCorrect={true}/>
+      <Question index={2} question='Inside which HTML element do we put the JavaScript?' options={['javascript', 'scripting', 'js', 'script']} isCorrect={true}/>
+      <Question index={3} question='Inside which HTML element do we put the JavaScript?' options={['javascript', 'scripting', 'js', 'script']} isCorrect={false}/>
+      <Question index={4} question='Inside which HTML element do we put the JavaScript?' options={['javascript', 'scripting', 'js', 'script']} isCorrect={false}/>
+      <Question index={5} question='Inside which HTML element do we put the JavaScript?' options={['javascript', 'scripting', 'js', 'script']} isCorrect={false}/>
+      <Question index={6} question='Inside which HTML element do we put the JavaScript?' options={['javascript', 'scripting', 'js', 'script']} isCorrect={false}/>
+      <Question index={7} question='Inside which HTML element do we put the JavaScript?' options={['javascript', 'scripting', 'js', 'script']} isCorrect={true}/>
+      <Question index={8} question='Inside which HTML element do we put the JavaScript?' options={['javascript', 'scripting', 'js', 'script']} isCorrect={true}/>
+    </div>
   );
 }
 const Question = (props) => {
   return (
-    <div className="question-block">
+    <div className={`question-block ${props.isCorrect ? 'question-correct' : 'question-incorrect'}`}>
       <div className="question-content">
         <span><b>{props.index}. {props.question}</b></span>
       </div>
       <div className="answers">
-        {props.options.map((option, index) => <input type="checkbox" name={`question-${props.index}`} value={index}>{option}</input>)}
+        {props.options.map((option, index) => (
+          <div className="option" key={`question${props.index}-option${index}`}>
+            <input type="radio" name={`question-${props.index}`} value={index} />
+            <label>{option}</label>
+          </div>
+        ))}
       </div>
-      <div className={`question-result ${props.isCorrect && 'correct'}`}>{props.isCorrect ? 'Your answer is Correct' : 'Your answer is Incorrect'}</div>
+      <div className={`question-result ${props.isCorrect ? 'correct' : 'incorrect'}`}>{props.isCorrect ? 'Your answer is Correct' : 'Your answer is Incorrect'}</div>
     </div>
   );
 }
